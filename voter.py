042a3967cbee9763b8f3a7e9842e4345c5418112
daniel1991gomes <daniel1991gomes@gmail.com>
2017-04-19 23:26:14 +0100

    message

---
+++ voter.py
@@ -0,0 +1,21 @@
+import socket
+import sys
+
+try:
+
+    # Send data
+    message = 'This is the message.  It will be repeated.'
+    print >> sys.stderr, 'sending "%s"' % message
+    sock.sendall(message)
+
+    amount_received = 0
+    amount_expected = len(message)
+
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print >> sys.stderr, 'received "%s"' % data
+
+finally:
+    print >> sys.stderr, 'closing socket'
+    sock.close()
